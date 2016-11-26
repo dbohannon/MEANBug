8799604ee5a9724ada055488f018f6c387c9a71a
david bohannon <dbohannon@cigital.com>
2016-11-25 23:54:12 -0500

    Added session management

--- server.js
+++ server.js
@@ -4,28 +4,33 @@ var serveStatic = require('serve-static');
 var bodyParser = require('body-parser')
 var mongo = require('mongodb').MongoClient;
 var unless = require('express-unless');
+var session = require('express-session');
 
 //include config file
 var config = require('./server.conf');
 
-//create express server
+//create express server and register global middleware
 var app = express();
 app.use(bodyParser.json());	//to support json bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 app.use(serveStatic(__dirname + '/public'));	//serve files in /public dir
+//note: session stored in memory only
+app.use(session({
+	secret: config.sessionSecret,
+	resave: true,
+	saveUninitialized: true,
+}));
 
 //bind to interface localhost:9000
 app.listen(9000, function(){
 	console.log("Server running on localhost, port %d.", this.address().port);
 });
 
-//TODO replace with express-session
-var session=false;
-
-function authenticate(user, pass, res){
-	//connect to MongoDB - TODO add auth
+function authenticate(user, pass, req, res){
+	//connect to MongoDB - auth not enabled 
+	//also, http interface enabled at http://localhost:28017/
 	mongo.connect('mongodb://localhost:27017/users', function(err, db){
 		if(err){ 
 			console.log('MongoDB connection error...');
@@ -37,8 +42,11 @@ function authenticate(user, pass, res){
 				return err;
 			}
 			if(result !== null){
-				session=true;
-				res.redirect('/secure/invoices');
+				//generate new session once logged in
+				req.session.regenerate(function(err){
+					req.session.authenticated = true;
+					res.redirect('/secure/invoices');
+				});
 			}
 			else
 				res.redirect('/');
@@ -49,7 +57,7 @@ function authenticate(user, pass, res){
 
 //If logged in, continue; else, redirect to index page
 var isLoggedIn = function(req, res, next){
-	if(session)
+	if(req.session.authenticated)
 		next();
 	else
 		res.redirect('/');	
@@ -58,20 +66,26 @@ var isLoggedIn = function(req, res, next){
 //add express-unless to isLoggedIn
 isLoggedIn.unless = unless;
 
-//apply isLoggedIn to routes
+//apply isLoggedIn to all routes beginning with /secure
 app.use(isLoggedIn.unless({path: /^(?!\/secure).*/}));
 
 //route - index page with login form
 app.get('/', function(req, res){
-	res.sendFile('./public/index.html', {root: __dirname})
+	res.sendFile('./index.html', {root: __dirname})
 });
 
 app.get('/secure/invoices', function(req, res){
-	res.sendFile('./public/invoices.html', {root: __dirname})
+	res.sendFile('./invoices.html', {root: __dirname})
+});
+
+app.get('/logout', function(req, res){
+	req.session.destroy(function(err){
+		res.redirect('/');	
+	});
 });
 
 app.post('/login', function(req, res){
-	authenticate(req.body.user, req.body.pass, res);
+	authenticate(req.body.user, req.body.pass, req, res);
 });
 
 app.post('/secure/query', function(req, res){
