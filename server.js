1f87f322829357095e08e19a3691121cdbe3353e
David Bohannon <david.bohannon.016@gmail.com>
2020-01-14 15:40:02 -0500

    Update server.js
    changed listening app port to 9001

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.use(session({
 	}
 }));
 
-//bind to interface localhost:9000
-app.listen(9000, function(){
+//bind to interface localhost:9001
+app.listen(9001, function(){
 	if(process.env.NODE_ENV === undefined)
 		process.env.NODE_ENV = 'development';
 	console.log("Server running on localhost, port %d in %s mode.", this.address().port, process.env.NODE_ENV);
